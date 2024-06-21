2025b86d17baeba4d05b362295f7014d0c3776d6
Adithya6ramesh <adithyaramesh66@gmail.com>
2024-06-21 12:28:25 +0530

    first commit

---
+++ OneDrive/Documents/NetBeansProjects/Online_Banking_System/src/main/java/com/mycompany/online_banking_system/Online_Banking_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.online_banking_system;
+
+/**
+ *
+ * @author hp
+ */
+public class Online_Banking_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
