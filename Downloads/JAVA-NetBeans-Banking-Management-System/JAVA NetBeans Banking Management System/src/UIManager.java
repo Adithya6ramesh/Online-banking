04a1782b8e6f0e95b939b6d2b0b95e9d8923b175
Adithya6ramesh <adithyaramesh66@gmail.com>
2024-06-25 00:49:39 +0530

    third commit

---
+++ Downloads/JAVA-NetBeans-Banking-Management-System/JAVA NetBeans Banking Management System/src/UIManager.java
@@ -0,0 +1,9 @@
+
+
+class UIManager {
+
+    static void setLookAndFeel(String comjtattooplafsmartSmartLookAndFeel) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
